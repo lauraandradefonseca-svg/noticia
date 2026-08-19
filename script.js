aec9a7304b23237337b8c7245834e0ee49fadfba
lauraandradefonseca-svg <laura.andrade.fonseca@escola.pr.gov.br>
2026-08-19 10:44:26 -0300

    mudnças

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+  // Preenche a data de publicação automaticamente com a data atual
+  const dataElemento = document.getElementById("data-publicacao");
+  const hoje = new Date();
+  
+  const opcoesData = { year: 'numeric', month: 'long', day: 'numeric' };
+  dataElemento.textContent = hoje.toLocaleDateString('pt-BR', opcoesData);
+});
